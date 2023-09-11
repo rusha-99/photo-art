734202f4b4583be0efa8382218d16974880c574e
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-11 13:30:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Gallery from './Gallery';
+
 import './gallery.css';
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Photo Art</h1>
+
       <h1 style={{ textAlign: 'center' }}>Tharusha Pathirana</h1>
 
       <Gallery />
